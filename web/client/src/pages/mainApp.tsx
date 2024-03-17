b9c290c88f9c0739a0d740db29c7e18250557b27
TYAtanasov21 <TYAtanasov21@codingburgas.bg>
2024-03-17 12:10:31 +0200

    Edit front-end

--- web/client/src/pages/mainApp.tsx
+++ web/client/src/pages/mainApp.tsx
@@ -1,88 +1,112 @@
-import React, {ChangeEvent, useEffect, useState} from 'react'
+import React, { ChangeEvent, useEffect, useState, useRef } from 'react';
 import Line from '../components/line';
-import { IoSend } from "react-icons/io5";
+import { IoSend } from 'react-icons/io5';
 import SidePanel from '../components/sidePanel';
 import CirclePhoto from '../components/circlePhoto';
-import { useLocation } from "react-router-dom";
-import { IoCheckmarkDoneOutline } from "react-icons/io5";
+import { useLocation } from 'react-router-dom';
+import { IoCheckmarkDoneOutline } from 'react-icons/io5';
 import axios from 'axios';
 
-const MainApp: React.FC = () =>{
-    const [isChatting, setIsChatting] = useState(true);
-    const location = useLocation();
-    const user = location.state.user;
+const MainApp: React.FC = () => {
+  const [isChatting, setIsChatting] = useState(true);
+  const location = useLocation();
+  const user = location.state.user;
 
-    const [messageDraft, setMessageDraft] = useState<string>('');
-    const [messages, setMessages] = useState<string[]>([]);
+  const [messageDraft, setMessageDraft] = useState<string>('');
+  const [messages, setMessages] = useState<string[]>([]);
+  const messagesEndRef = useRef<HTMLDivElement>(null);
 
-    const handleInputChange = (event:ChangeEvent<HTMLInputElement>) => {
-        setMessageDraft(event.target.value);
-        console.log(messageDraft);
-    };
+  const scrollToBottom = () => {
+    if (messagesEndRef.current) {
+      messagesEndRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
 
-    const handleSendClick = async () => {
-        const tempMessage = messageDraft;
-        setMessageDraft('');
-        const response = await axios.post("http://localhost:3001/ML/checkMessage", {message: tempMessage});
-        console.log(response.data, tempMessage);
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setMessageDraft(event.target.value);
+  };
 
-        if(response.data.isBullying == false){
-            if (messageDraft.trim() !== '') {
-                setMessages([...messages, tempMessage]);
-                setMessageDraft('');
-            }
-        }
-        else setMessages([...messages, "This message has been flagged as cyberbullying."]);
-        }
+  const handleSendClick = async () => {
+    sendMessage();
+  };
 
-
-    useEffect(()=>{
-        console.log(user.username, typeof (user.username));
+  const sendMessage = async () => {
+    const tempMessage = messageDraft;
+    setMessageDraft('');
+    const response = await axios.post('http://localhost:3001/ML/checkMessage', {
+      message: tempMessage,
     });
-    return (  
-        <div className = "flex flex-row bg-gradient-to-b from-sky-700 to-sky-500">
-                <SidePanel username={user.username}/>
-            {!isChatting && 
-            <div className = "flex flex-col bg-white mx-auto my-auto p-44 rounded-xl items-center font-sans text-gray-500 font-bold">
-                    <h1>No Chats</h1>
-                    <p>Select a conversation to start</p>
-            </div>}
-            {isChatting && 
-            <div className = "flex flex-col bg-gradient-to-b from-sky-300 to-indigo-400 w-full ml-0.5 justify-between">
-                <div className = "flex flex-col justify-start p-3">
-                    <div className = "flex flex-row items-center mb-1">
-                        <CirclePhoto image_url = {require('../assets/Ivan-Dochev.jpg')} size = {50} className = "mr-4 text-green-400"/>
-                    <h1 className='font-serif font-bold text-xl'>Ivan Dochev</h1>
-                    </div>
-                    <Line color = "black"/>
-                        <div>
-                            {messages.map((message, index) => (
-                                <div key={index} className="flex flex-col text-end mx-auto mr-0 w-fit">
-                                    {message != "This message has been flagged as cyberbullying." ?
-                                    <div className="flex flex-row bg-blue-600 p-2 rounded-2xl mb-1 font-sans text-white px-4 items-center">
-                                        {message}
-                                        <IoCheckmarkDoneOutline className='ml-3'/>
-                                    </div>
-                                    :
-                                    <div className="flex flex-row bg-red-600 p-2 rounded-2xl mb-1 font-sans text-white px-4 items-center">
-                                        {message}
-                                        <IoCheckmarkDoneOutline className='ml-3'/>
-                                    </div>
-                                    }
-                                </div>
-                            ))}
-                        </div>
-                    </div>
-                    <div>
-                        <div className = "p-4 flex flex-row justify-center">
-                        <input onChange={handleInputChange} className = "bg-gray-200 w-11/12 py-2 outline-none rounded-xl px-3" placeholder='Type a message...' value={messageDraft}></input>
-                        <button onClick={handleSendClick}className = "ml-7 rounded-2xl bg-sky-400 px-4 py-2"><IoSend color='white' size = "20px"/></button>
-                        </div>
-                    </div>
-            </div>}
-            
+
+    if (response.data.isBullying === false) {
+      if (messageDraft.trim() !== '') {
+        setMessages([...messages, tempMessage]);
+        setMessageDraft('');
+      }
+    } else setMessages([...messages, 'This message has been flagged as cyberbullying.']);
+  };
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      handleSendClick();
+    }
+  };
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages]);
+
+  return (
+    <div className="flex flex-auto bg-gradient-to-b from-sky-700 to-sky-500">
+      <SidePanel username={user.username} />
+      {!isChatting && (
+        <div className="flex flex-col bg-white mx-auto my-auto p-44 rounded-xl items-center font-sans text-gray-500 font-bold">
+          <h1>No Chats</h1>
+          <p>Select a conversation to start</p>
         </div>
-    );
-}
+      )}
+      {isChatting && (
+        <div className="flex flex-col bg-gradient-to-b from-sky-300 to-indigo-400 w-full ml-0.5 justify-between">
+          <div className="flex flex-col justify-start p-3" style={{ overflow: 'visible' }}>
+            <div className="flex flex-row items-center mb-1">
+              <CirclePhoto image_url={require('../assets/Ivan-Dochev.jpg')} size={50} className="mr-4 text-green-400" />
+              <h1 className="font-serif font-bold text-xl">Ivan Dochev</h1>
+            </div>
+            <Line color="black" />
+            <div>
+              {messages.map((message, index) => (
+                <div key={index} className="flex flex-col text-end mx-auto mr-0 w-fit">
+                  <div
+                    className={`flex flex-row p-2 rounded-2xl mb-1 font-sans text-white px-4 items-center ${
+                      message !== 'This message has been flagged as cyberbullying.' ? 'bg-blue-600' : 'bg-red-600'
+                    }`}
+                  >
+                    {message}
+                    <IoCheckmarkDoneOutline className="ml-3" />
+                  </div>
+                </div>
+              ))}
+              <div ref={messagesEndRef} />
+            </div>
+          </div>
+          <div className = "bottom-0">
+            <div className="p-4 flex flex-row justify-center">
+              <input
+                onChange={handleInputChange}
+                onKeyDown={handleKeyDown}
+                className="bg-gray-200 w-11/12 py-2 outline-none rounded-xl px-3"
+                placeholder="Type a message..."
+                value={messageDraft}
+              ></input>
+              <button onClick={handleSendClick} className="ml-7 rounded-2xl bg-sky-400 px-4 py-2">
+                <IoSend color="white" size="20px" />
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+      </div> 
+  );
+};
 
 export default MainApp;
